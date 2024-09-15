32bcbd89a29e7d1a405387780b7b50fdbac0b853
Al Gabriel Orig <jaworski.orig@gmail.com>
2024-09-16 01:34:31 +0800

    reconstruct

--- prelim_project/lib/providers.dart
+++
@@ -1,7 +0,0 @@
-import 'package:riverpod/riverpod.dart';
-import 'src/attendance_service.dart';
-
-// Provider for AttendanceService
-final attendanceServiceProvider = Provider<AttendanceService>((ref) {
-  return AttendanceService();
-});
