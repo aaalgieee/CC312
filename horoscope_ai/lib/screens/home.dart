aa9310991b95cfd61cfbae5f487e99d2871f5d82
Al Gabriel <jaworski.orig@gmail.com>
2024-11-11 03:30:54 +0800

    chore: remove connect_plus package

--- horoscope_ai/lib/screens/home.dart
+++ horoscope_ai/lib/screens/home.dart
@@ -1,12 +1,10 @@
 // ignore_for_file: prefer_const_constructors, unused_field
 
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'theme.dart';
 import 'reading.dart';
 import '/providers.dart';
-import 'dart:io';
 
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
